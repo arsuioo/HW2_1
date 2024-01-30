adb4c230db1dbf5497ea3c5c9389ad41701dd8ca
Arsh_174 <74has@mail.ru>
2024-01-30 22:08:44 +0500

    task1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int x = 26148; // стоимость билета
+        int y = 20; // одна бонусная миля
+        int z = x/y; //бонусные мили
+        System.out.println(z);
+        }
+}
